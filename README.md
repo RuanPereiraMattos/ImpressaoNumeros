9ffa3bdd3fc0bdd7ce1f4a7723d49dc0a69bbea3
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-14 20:06:52 -0300

    Primeira versão

---
+++ README.md
@@ -0,0 +1,1 @@
+# ImpressaoNumeros
---
+++ src/main/java/com/ruanmattos/impressaonumeros/main/ImpressaoNumeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ruanmattos.impressaonumeros.main;
+
+/**
+ *
+ * @author aluno
+ */
+public class ImpressaoNumeros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
