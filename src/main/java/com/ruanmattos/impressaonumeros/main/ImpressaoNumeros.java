b2eb950d5dca8c6560143e57ab66f5fa7cd50347
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-14 20:09:12 -0300

    Implementação realizada com sucesso

--- src/main/java/com/ruanmattos/impressaonumeros/main/ImpressaoNumeros.java
+++ src/main/java/com/ruanmattos/impressaonumeros/main/ImpressaoNumeros.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.ruanmattos.impressaonumeros.main;
 
 /**
+ * Impressão dos Numeros de 1 a 10
  *
- * @author aluno
+ * @author Ruan Pereira Mattos
  */
 public class ImpressaoNumeros {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bem vindo(a) ao programa que irá imprimir na tela do número 1 ao número 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
